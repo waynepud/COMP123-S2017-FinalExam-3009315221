e9c5e1574fce3d52b1c3bb1d195e2629e00a0598
Eva <Eva@DESKTOP-CDRCGMN>
2017-08-17 12:36:44 -0400

    Add project files.

---
+++ COMP123-S2017-FinalExam-3009315221/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name:
+ * Date:
+ * StudentID:
+ * Description:
+ * Version:
+ */
+
+namespace COMP123_S2017_FinalExam_3009315221
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PickHighestCardForm());
+        }
+    }
+}
---
+++ COMP123-S2017-FinalExam-3009315221/Suit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Tom Tsiliopoulos
+ * Date: July 25, 2017
+ * Description: This is the Suit enum
+ * Version: 0.1 - Created the Suit enum
+ */
+
+namespace COMP123_S2017_FinalExam_3009315221
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
